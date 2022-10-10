ac5919c4565eaf083178a0b0b22616933e32f73b
Omar Khalil <omarkhalil770@gmail.com>
2022-10-10 22:48:11 +0200

    initial commit

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
+export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
