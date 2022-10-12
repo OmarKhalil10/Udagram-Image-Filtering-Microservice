32bb2f6fd48fc5048eca6c4bddaf8699e2c0083a
Omar Khalil <omarkhalil770@gmail.com>
2022-10-12 23:25:20 +0200

    Added reverseproxy external ip address and update travis.ynl file to build frontend with our version

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab7b1d039ea5144538303bf01900a851-367537129.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
